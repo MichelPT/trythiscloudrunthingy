b49c42d8e5cc990e60cfc99c8e8ace641467df5d
Michel <103488422+MichelPT@users.noreply.github.com>
2024-05-08 00:01:59 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http')
 const server = http.createServer((req, res)=>{
     console.log('New connection')
-    res.end('Nice bruv')
+    res.end('This is the testing page and welcome to the testing page that doesnt actually have any testing since the developer who made this is actually in a pretty tight schedule')
 })
 const PORT = process.env.PORT || 8080
 server.listen(PORT, ()=>console.log('nging...'))
